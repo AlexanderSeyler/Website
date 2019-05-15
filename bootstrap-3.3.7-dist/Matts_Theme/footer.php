a6f309532b03f16604317b84adfe828578868ce2
AlexanderSeyler <alexanderseyler@gmail.com>
2019-05-15 14:19:03 +0800

    php files added
    php files added

---
+++ bootstrap-3.3.7-dist/Matts_Theme/footer.php
@@ -0,0 +1,36 @@
+<footer class="container-fluid footerbg">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4 leftfoot">
+                <h1>Follow Matt on social media to keep updated!</h1>
+                <a href="#"><img src="images/facebook.png" alt="facebook"></a>
+                <a href="#"><img src="images/instagram.png" alt="instagram"></a>
+
+            </div>
+
+            <div class="col-md-4 middlefoot">
+                <img src="images/logo.png" alt="logo" class="footerlogo" href="#">
+
+
+            </div>
+
+            <div class="col-md-4 rightfoot">
+                <h1> Contact Information</h1>
+                <p>Email: bumblemcpop@gmail.com</p>
+                <p>Phone: 123456789</p>
+
+
+
+            </div>
+
+        </div>
+        <!--row-->
+
+    </div>
+    <!--container-->
+</footer>
+
+</body>
+
+</html>
+<?php wp_footer(); ?>
